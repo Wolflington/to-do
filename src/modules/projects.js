df62c0b413568d65d7d1b2a33ac502c632a7adeb
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-12-12 13:13:42 +0800

    Create event listener that adds <a> DOM elements to project-list

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,5 +1,10 @@
+//Create a factory function similar to a Class constructor for projects
+function projectProperties(projectTitle) {
+    return { projectTitle }
+}
+
 //Create a function that adds project tabs
-function createProjectTabs() {
+export function createProjectTabs() {
     //Create <a> elements
     const projects = document.createElement('a');
     projects.classList.add('projects');
@@ -11,11 +16,13 @@ function createProjectTabs() {
     console.log('I am clicked from event listener in projects.js')
 }
 
+//Create a function that displays an input (text) to name the project
+
+// //Create a function that changes tab
+function changeTabs(currentButton) {
+
+}
+
 //Create event listeners for adding projects with add project button
 const projectsBtn = document.querySelector('.add-project-btn');
 projectsBtn.addEventListener('click', createProjectTabs);
-
-// //Create a function that changes tab
-// function changeTabs() {
-
-// }
