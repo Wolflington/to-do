ae2df0e7051a789ff1ee8496d7d547b0bfd5bb9d
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-12-15 10:14:50 +0800

    Call allTasksContent() to load all tasks on default

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import {toDoProperties, createToDo} from './modules/tasks';
+import { displayToDo, changeTabContent, allTasksContent } from './modules/UI';
+import { createProjectTabs } from './modules/projects';
 
-console.log('I am logged from index.js!');
+const todoManager = createToDo();
+todoManager.addToDo('Go to the gym', 'Workout session', '12/6/2023', 'Medium', true);
+
+function initializeWebsite() {
+    allTasksContent(); //Load All Tasks content upon initialization
+    changeTabContent();
+}
+
+initializeWebsite();
