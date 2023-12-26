c65881829ab7202871ee715b7cc1b01a88418902
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-12-26 20:08:20 +0800

    Add event listener to load printed projects

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,88 +1,83 @@
 import { createToDo } from './tasks';
 
-//Create a factory function similar to a Class constructor for projects
+// Create a factory function similar to a Class constructor for projects
 function projectProperties(projectTitle) {
-    return { projectTitle }
+  return { title: projectTitle, tasks: [] }; // Modified: Added tasks property to store tasks for each project
 }
 
-//Create a function that adds project tabs
+// Create a function that adds project tabs
 export function createProjectTabs() {
-    let projectList = JSON.parse(localStorage.getItem('toDoList')) || []; //Retrieves 'toDoList' key then parses it from JSON array into JavaScript array. 
+  let projectList = localStorage.getItem('toDoList') ? JSON.parse(localStorage.getItem('toDoList')) : [];
 
-    const getProjectList = () => {
-        return projectList;
-    }
+  const getProjectList = () => {
+    return projectList;
+  };
 
-    const getCreateToDo = createToDo();
-    const getList = getCreateToDo.getToDo;
+  const getCreateToDo = createToDo();
+  const getList = getCreateToDo.getToDo();
 
-    function addProjects(projectTitle) {
-        let newProject = { projectTitle };
-        projectList.push(newProject);
-        // projectList.push(getList);
-        localStorage.setItem('toDoList', JSON.stringify(projectList)); //Converts the object into strings
-    }
+  function addProjects(projectTitle) {
+    // Store project's title and array of tasks
+    let newProject = {
+      title: projectTitle,
+      tasks: [], // Initialize an empty tasks array for the new project
+    };
+    projectList.push(newProject);
+    
+  }
 
-    return { projectList, getProjectList, addProjects };
+  return { projectList, getProjectList, addProjects };
 }
-
-// const test = createProjectTabs();
-// test.addProjects('Test!');
 
 function displayProjects() {
-    //Get the array for projects list
-    const getProjects = createProjectTabs();
-    const getProjList = getProjects.getProjectList();
-    const projectsList = document.querySelector('.projects-list');
+  const getProjects = createProjectTabs();
+  const getProjList = getProjects.getProjectList();
+  const projectsList = document.querySelector('.projects-list');
 
-    const printProjects = () => {
-        projectsList.innerHTML = '';
+  const printProjects = () => {
+    projectsList.innerHTML = '';
 
-        getProjList.forEach((project, index) => {
-            const projects = document.createElement('p');
-            projects.classList.add('projects');
-            projects.textContent = JSON.stringify(project);
+    getProjList.forEach((project, index) => {
+      const projects = document.createElement('p');
+      projects.classList.add('projects');
+      projects.textContent = project.title;
 
-            //Append to project lists
-            const projectsList = document.querySelector(".projects-list");
-            projectsList.append(projects);
-        });
-    }
+      // Append to project lists
+      const projectsList = document.querySelector('.projects-list');
+      projectsList.append(projects);
+    });
+  };
 
-    //Create a method that pushes project into array
-    const addProj = (projectTitle) => {
-        getProjList.push(projectTitle);
-        console.log(getProjList);
-    }
+  const addProj = (projectTitle) => {
+    getProjList.push(projectTitle);
+    localStorage.setItem('toDoList', JSON.stringify(getProjList)); // Converts the object into strings
+    console.log(getProjList);
+  };
 
-    const clearInput = () => {
-        document.getElementById('project-title').value = '';
-    }
+  const clearInput = () => {
+    document.getElementById('project-title').value = '';
+  };
 
-    return { printProjects, addProj, clearInput }
+  return { printProjects, addProj, clearInput };
 }
 
 const displayProjectsObj = Object.create(displayProjects());
+const getProjects = createProjectTabs();
 
-//Event listener for adding projects
+// Ensure the projects are printed when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  displayProjectsObj.printProjects();
+});
+
 const projectForm = document.getElementById('project-form');
-//Attach the form to an event listener
-projectForm.addEventListener('submit', function(e) {
-    //Default action should not be taken if the event is not explicitly handled
-    e.preventDefault();
-    //Get the value of the property from input
-    const projectTitle = document.getElementById('project-title').value;
-    const getProjects = createProjectTabs();
+projectForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const projectTitle = document.getElementById('project-title').value;
 
-    //Create a new projectProperties instance
-    // const newCreateProject = createProjectTabs();
-    // let getProjList = newCreateProject.getProjectList();
-    const project = projectProperties(projectTitle)
-    //Push the new instance into addProjects
+  const project = projectProperties(projectTitle);
+  displayProjectsObj.addProj(project);
+  displayProjectsObj.printProjects();
+  displayProjectsObj.clearInput();
 
-    displayProjectsObj.addProj(project);
-    displayProjectsObj.printProjects();
-    displayProjectsObj.clearInput();
-    
-    getProjects.addProjects(projectTitle);
+  getProjects.addProjects(projectTitle);
 });
